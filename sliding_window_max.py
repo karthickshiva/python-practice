00b38fb6a05829dcdb063ed86c9e1627b801897e
karthick.shiva <karthick.shiva@zohocorp.com>
2019-03-23 18:40:50 +0530

    Sliding window maximum

---
+++ sliding_window_max.py
@@ -0,0 +1,14 @@
+from collections import deque
+for __ in range(int(input())):
+    n, k = list(map(int, input().split()))
+    a = list(map(int, input().split()))
+    d = deque()
+    for i in range(n):
+        while d and d[0]<=i-k:
+            d.popleft()
+        while d and a[d[-1]]<a[i]:
+            d.pop()
+        d.append(i)
+        if i>=k-1:
+            print(a[d[0]], end=" ")
+    print()
