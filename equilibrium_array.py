6f767618b1bf0cb5027468dd522a63b698851763
karthick.shiva <karthick.shiva@zohocorp.com>
2019-03-23 12:01:48 +0530

    Equilibrium Array

--- equilibrium_array.py
+++ equilibrium_array.py
@@ -1,3 +1,8 @@
+'''
+Given an array A of N positive numbers. The task is to find the position where equilibrium first 
+occurs in the array. Equilibrium position in an array is a position 
+such that the sum of elements before it is equal to the sum of elements after it.
+'''
 for i in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
